fea884cdf13486dfc757aef4c54576b59175684d
AHMAD-DOMA <ahmad_201900065@fci.helwan.edu.eg>
2022-04-02 15:18:37 +0200

    adding animations for hit and die and some logic to the game related to health and score

--- Assets/scripts c#/PlayerMovement.cs
+++ Assets/scripts c#/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     private int health = 3;
     private Boolean gameOver = false;
     private Animator anim;
+    private int RATIO_OF_SCORE_TO_HEALTH = 7;
+    private int MaximumHealth = 3;
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -18,7 +20,18 @@ public class PlayerMovement : MonoBehaviour
 
 
     void Update()
-    {     
+    {
+        if (Score >= RATIO_OF_SCORE_TO_HEALTH && health < MaximumHealth)
+        {
+            for (int i = 0; i < Score/RATIO_OF_SCORE_TO_HEALTH; i++)
+            {
+                Score -= RATIO_OF_SCORE_TO_HEALTH;
+                health += 1;
+            }
+            print("Score = " + Score);
+            print("Health = " +health);
+        }
+
         anim.SetBool("walking",false);
         if(!gameOver){
             SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
@@ -50,8 +63,8 @@ public class PlayerMovement : MonoBehaviour
    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("gim")){
-            Debug.Log(collision.name);
-            //Destroy(collision.gameObject);
+            //Debug.Log(collision.name);
+            Destroy(collision.gameObject);
         }
     }
 
@@ -62,25 +75,36 @@ public class PlayerMovement : MonoBehaviour
             anim.SetTrigger("dig");
 
             Destroy(collision.gameObject);
-            Score += 100;
-            print(Score);
-            
-
-
+            Score += 1;
+            print("Score = " + Score);
         }
-        if (collision.gameObject.CompareTag("stone")||collision.gameObject.CompareTag("enemy")||collision.gameObject.CompareTag("Fire"))
+        if (collision.gameObject.CompareTag("stone")||collision.gameObject.CompareTag("enemy"))
         {
             
             if(health==0){
                 if(!gameOver){
-                    gameOver = true;//disabled temprorey for easy debuging
-                    anim.SetTrigger("die");
+                    //gameOver = true;//disabled temprorey for easy debuging
+                    //anim.SetTrigger("die");//disabled temprorey for easy debuging
                     print("Game Over!");
+                    return;
                 }
             }
-            health -= 1;
+            if(!gameOver){
+                health -= 1;
+                anim.SetTrigger("hit");
+                print("Health = " +health);
+            }
         }
 
+        if (collision.gameObject.CompareTag("Fire"))
+        {
+            health = 0;
+            if(!gameOver){
+                //gameOver = true;//disabled temprorey for easy debuging
+                //anim.SetTrigger("die");//disabled temprorey for easy debuging 
+                print("Game Over!");
+            }
+        }
     }
 
 }
